fc700c9a998a63f597f7cd7d79859283c449a004
uliedberg <ulf@alfhild.io>
2017-08-24 13:27:33 +0200

    add API GET call in iframe

--- src/api.js
+++ src/api.js
@@ -4,20 +4,29 @@ const cookies = require('./cookies.js');
 
 const logger = bunyan.createLogger({name: "api-middleware"})
 
+const paths = {
+  '/redirect': function (ctx, opts) {
+    const { cookieName, domain } = opts;
+    const redirectUrl = ctx.request.query['url'];
+    if (!redirectUrl) ctx.throw(400, 'no "url" query param', { path: ctx.path, method: ctx.method });
+    logger.info({ hostname: ctx.hostname, redirect_url: redirectUrl }, 'will redirect (303)')
+
+    ctx.status = 303;
+    ctx.set('location', redirectUrl);
+    cookies.setUuid(ctx, cookieName, domain);
+  },
+  '/ping': function (ctx) {
+    logger.info({ hostname: ctx.hostname }, 'will respond to ping');
+    ctx.status = 200;
+    ctx.body = { message: 'pong' };
+  }
+};
+const errorFun = function (ctx) {
+  ctx.throw(400, 'unsupported', { path: ctx.path, method: ctx.method });
+};
+
 module.exports = function (opts = {}) {
-  const { cookieName, domain } = opts;
-
   return async (ctx, next) => {
-    if (ctx.path == '/redirect') {
-      const redirectUrl = ctx.request.query['url'];
-      if (!redirectUrl) ctx.throw(400, 'no redirect-url query param', { path: ctx.path, method: ctx.method });
-      logger.info({ hostname: ctx.hostname, redirect_url: redirectUrl }, 'will redirect (303)')
-
-      ctx.status = 303;
-      ctx.set('location', redirectUrl);
-      cookies.setUuid(ctx, cookieName, domain);
-    } else {
-      ctx.throw(400, 'unsupported', { path: ctx.path, method: ctx.method });
-    }
+    (paths[ctx.path] || errorFun)(ctx, opts);
   };
-}
+};
