c648b1211faf88522097dac0adce1a717f9b2a30
uliedberg <ulf@alfhild.io>
2017-08-25 15:09:03 +0200

    add redirect user interaction alternatives

--- src/api.js
+++ src/api.js
@@ -9,13 +9,12 @@ const logger = bunyan.createLogger({name: "api-middleware"})
 const paths = {
   '/redirect': function (ctx, opts) { // /redirect?url={url}
     const { cookieName, domain } = opts;
-    const redirectUrl = ctx.request.query['url'];
-    if (!redirectUrl) ctx.throw(400, 'no "url" query param', { path: ctx.path, method: ctx.method });
-    logger.info({ hostname: ctx.hostname, redirect_url: redirectUrl }, 'will redirect (303)')
-
-    ctx.status = 303;
-    ctx.set('location', redirectUrl);
+    redirect(ctx);
     cookies.setUuid(ctx, cookieName, domain);
+  },
+  '/bouncer_redirect': function (ctx, opts) {
+    logger.info({ hostname: ctx.hostname }, 'bouncer_redirect, ignoring body for now');
+    redirect(ctx);
   },
   '/cookie': function (ctx, opts) { // /cookie/{uuid}
     const { cookieName } = opts;
@@ -27,6 +26,15 @@ const paths = {
 const errorFun = function (ctx) {
   ctx.throw(400, 'unsupported', { path: ctx.path, method: ctx.method });
 };
+
+function redirect(ctx) {
+  const redirectUrl = ctx.request.query['url'];
+  if (!redirectUrl) ctx.throw(400, 'no "url" query param', { path: ctx.path, method: ctx.method });
+  logger.info({ hostname: ctx.hostname, redirect_url: redirectUrl }, 'will redirect (303)')
+
+  ctx.status = 303;
+  ctx.set('location', redirectUrl);
+}
 
 module.exports = function (opts = {}) {
   return async (ctx, next) => {
